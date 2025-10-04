06b6cd3940cf95a1ea3ea310592465ebe7b2738e
luisfps <luisssfps@gmail.com>
2025-10-04 14:42:38 -0500

    agregando los enums

---
+++ src/models/OrderStatus.java
@@ -0,0 +1,9 @@
+package models;
+
+public enum OrderStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    SERVED,
+    CANCELED
+}
---
+++ src/models/PaymentMethod.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum PaymentMethod {
+    CASH,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    BANK_TRANSFER
+}
