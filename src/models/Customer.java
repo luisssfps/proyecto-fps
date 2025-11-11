13524564d0846fb2f2a92cb4400de6a37827eef0
Luis Felipe Polo Sanchez <luisssfps@gmail.com>
2025-11-11 09:21:20 -0500

    arreglando archivo Customer.java que no tenia nada

---
+++ src/models/Customer.java
@@ -0,0 +1,36 @@
+package models;
+
+public class Customer {
+  private String name;
+  private String contactInfo;
+
+  public Customer(String name, String contactInfo) {
+    this.name = name;
+    this.contactInfo = contactInfo;
+  }
+
+  // Getters and Setters
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getContactInfo() {
+    return contactInfo;
+  }
+
+  public void setContactInfo(String contactInfo) {
+    this.contactInfo = contactInfo;
+  }
+
+  @Override
+  public String toString() {
+    return "Customer{" +
+        "name='" + name + '\'' +
+        ", contactInfo='" + contactInfo + '\'' +
+        '}';
+  }
+}
